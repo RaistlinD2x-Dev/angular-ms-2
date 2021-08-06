5fa8ce62c576cfc7929edbc95a9067bd5621f3c0
Jesse Richey <RaistlinD2x.dev@outlook.com>
2021-08-06 13:44:56 -0500

    setup template structure for shopping list and shopping list model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,9 @@
 export class Recipe {
-  public name: string; 
-  public description: string;
-  public imagePath: string; 
+ 
+  constructor(
+    public name: string, 
+    public description: string, 
+    public imagePath: string
+    ) {}
 
-  constructor(name: string, description: string, imagePath: string) {
-    this.name = name
-    this.description = description
-    this.imagePath = imagePath
-  }
 }
