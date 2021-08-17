e522ee7b389558bfae928b7661c16d4ab9d0e090
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-08-17 17:35:25 -0500

    setup recipes services and migrated recipes array

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test.',
+      'https://www.simplyrecipes.com/thmb/YSlSLYrnOBfkzE3rD_uMSnA8dlA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-LEAD-3-8aa37af554cf4445a1e3e54fdc66e974.jpg'
+    ),
+    new Recipe(
+      'Another Recipe',
+      'This is a test.',
+      'https://www.simplyrecipes.com/thmb/YSlSLYrnOBfkzE3rD_uMSnA8dlA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-LEAD-3-8aa37af554cf4445a1e3e54fdc66e974.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    // arrays are reference objects, use empty slice to create copy
+    return this.recipes.slice();
+  }
+}
